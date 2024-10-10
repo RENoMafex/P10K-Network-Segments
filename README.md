b9bd3b723b1c03f322bf464498861fea8a70c6cf
RENoMafex <Mafexxx@aol.com>
2024-10-11 00:04:02 +0200

    Changed README and some layout things

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # P10K-Network-Segments
-A few usefull (network related @romkatv/Powerlevel10k prompt segments.
+A few usefull (network related) @romkatv/Powerlevel10k prompt segments.
+
+## Usage
+
+Just download ``Networksegments.zsh`` to a place where you want, then add ``source /path/to/Networksegments.zsh`` to your ``.p10k.zsh`` and add the segments you want to use either to the left or the right prompt.
+
+### Segments
+- my\_wired\_ip
+- my\_wifi\_ip
+- my\_if\_count
--- networksegments.zsh
+++ networksegments.zsh
@@ -1,60 +1,31 @@
 # MY WIRED IP
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_FOREGROUND=38							# Text color
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_FOREGROUND='#ffffff'		# Text color if no wired IF is connected
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_BACKGROUND='#aa1100'		# Segment color if no wired IF is connected
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_SHOWIFNAME=false						# Show the name of the IF
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_SHOWUNCONNECTED=false					# Show segment even if no wired IF is connected
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_PREFIX='󰍸 '							# Prefix for the wired segment
-  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_PREFIX='󰍸'				# Prefix for the wired segment if no wired IF is connected
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_FOREGROUND=				38			# Text color
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_FOREGROUND=	'#ffffff'	# Text color if no wired IF is connected
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_BACKGROUND=	'#aa1100'	# Segment color if no wired IF is connected
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_SHOWIFNAME=				false		# Show the name of the IF
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_SHOWUNCONNECTED=			false		# Show segment even if no wired IF is connected
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_PREFIX=					'󰍸 '		# Prefix for the wired segment
+  typeset -g POWERLEVEL9K_MY_WIRED_IP_UNCONNECTED_PREFIX=		'󰍸'			# Prefix for the wired segment if no wired IF is connected
 # MY WIFI IP
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_FOREGROUND=38							# Text color
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_FOREGROUND='#ffffff'		# Text color if no wifi IF is connected
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_BACKGROUND='#aa1100'		# Segment color if no wifi IF is connected 
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_SHOWIFNAME=false						# Show the name of the IF
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_SHOWUNCONNECTED=true					# Show segment even if no wifi
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_PREFIX=' '							# Prefix for the wifi segment
-  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_PREFIX=''					# Prefix for the wifi segment if no wifi IF is connected
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_FOREGROUND=				38			# Text color
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_FOREGROUND=	'#ffffff'	# Text color if no wifi IF is connected
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_BACKGROUND=	'#aa1100'	# Segment color if no wifi IF is connected 
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_SHOWIFNAME=				false		# Show the name of the IF
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_SHOWUNCONNECTED=			true		# Show segment even if no wifi
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_PREFIX=					' '		# Prefix for the wifi segment
+  typeset -g POWERLEVEL9K_MY_WIFI_IP_UNCONNECTED_PREFIX=		''			# Prefix for the wifi segment if no wifi IF is connected
 # MY IF COUNT
-  typeset -g POWERLEVEL9K_MY_IF_COUNT_FOREGROUND=38							# Text color
-  typeset -g POWERLEVEL9K_MY_IF_COUNT_PREFIX='#'							# Prefix for the Count segment
-  typeset -g POWERLEVEL9K_MY_IF_COUNT_MAXUSUAL=2							# If more than this number of wired/wifi IF are connected,
+  typeset -g POWERLEVEL9K_MY_IF_COUNT_FOREGROUND=				38			# Text color
+  typeset -g POWERLEVEL9K_MY_IF_COUNT_PREFIX=					'#'			# Prefix for the Count segment
+  typeset -g POWERLEVEL9K_MY_IF_COUNT_MAXUSUAL=					2			# If more than this number of wired/wifi IF are connected,
   																			#	segment will turn red
-  typeset -g POWERLEVEL9K_MY_IF_COUNT_UNUSUAL_FOREGROUND='#cc2222'			# Color of the segment if 0 or more than the number above
+  typeset -g POWERLEVEL9K_MY_IF_COUNT_UNUSUAL_FOREGROUND=		'#cc2222'	# Color of the segment if 0 or more than the number above
   																			#	are connected
 
 
 #####################
 # BEGIN OF SEGMENTS #
 #####################
-
-function prompt_my_uptime () {
-	local uptime_seconds_in=$( /bin/cat /proc/uptime | /bin/awk '{print int($1)}' )
-	local uptime_hours=$(( uptime_seconds_in / 3600 ))
-	local uptime_minutes=$(( (uptime_seconds_in % 3600) / 60 ))
-	local uptime_seconds=$(( uptime_seconds_in % 60 ))
-
-	if [[ $POWERLEVEL9K_MY_UPTIME_SHOW_SECONDS == true ]]; then
-		if (( uptime_minutes < 10 )); then
-			if (( uptime_seconds < 10 )); then
-				p10k segment -t "$uptime_hours:0$uptime_minutes:0$uptime_seconds"
-			else
-				p10k segment -t "$uptime_hours:0$uptime_minutes:$uptime_seconds"
-			fi
-		else
-			if ((uptime_seconds < 10 )); then
-				p10k segment -t "$uptime_hours:$uptime_minutes:0$uptime_seconds"
-			else
-				p10k segment -t "$uptime_hours:$uptime_minutes:$uptime_seconds"
-			fi
-		fi
-	else
-		if (( uptime_minutes < 10 )); then
-			p10k segment -t "$uptime_hours:0$uptime_minutes"
-		else
-			p10k segment -t "$uptime_hours:$uptime_minutes"
-		fi
-	fi
-}
 
 function prompt_my_wired_ip () {
 	local interface=$( /bin/ip link show | /bin/grep -Eo '^[0-9]+: ([Ee]\w+):' | /bin/grep -Eo '[Ee]\w+' | /bin/head -n 1 )
